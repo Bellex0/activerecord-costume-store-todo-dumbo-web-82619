2803dcecd12edbb70b2033e5d9043a307b1758ba
Belle P <bellepx3@gmail.com>
2019-09-07 20:12:05 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :size
+      t.boolean :
       t.timestamps
     end
   end
