9ceae46e4e7c1952315abcb57a908b16a39fa59b
Belle P <bellepx3@gmail.com>
2019-09-07 20:09:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,13 @@
 # Create your costume_stores migration here
 
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
